c829ac97f9cc8fb4e617ddf419457cce0df78a7a
danielapuerta <danielapuerta151@gmail.com>
2023-06-26 20:51:47 +0100

    add user table with values

--- seeds/user_table.js
+++ seeds/user_table.js
@@ -1,13 +1,9 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
- */
-exports.seed = async function(knex) {
-  // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+
+exports.seed = async knex => {
+  await knex('users').del()
+  await  knex('users').insert([
+        {username: 'leo', password_hash: "leo_password"},
+        {username: 'mark', password_hash: "mark_password"}, 
+        {username: 'tom', password_hash: "tom_password"},
+      ]);
 };
