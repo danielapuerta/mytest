19f4a88784430058cb367ae099011fd54ae9a583
danielapuerta <danielapuerta151@gmail.com>
2023-06-26 17:57:27 +0100

    deleting for practice

--- routes/user.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt')
-const db = require("../db/db")
-
-module.exports = router;
