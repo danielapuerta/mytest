2c62a8ab14977c96024f45daf59d551ab3fa8690
danielapuerta <danielapuerta151@gmail.com>
2023-06-26 14:24:57 +0100

    export router from user.js and import it to index.js.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require("body-parser")
 const cors = require('cors')
+const userRouter = require('./routes/user')
 
 const app = express();
 
@@ -8,5 +9,7 @@ app.use(bodyParser.json())
 app.use(cors())
 
 const port = 5000
+
+app.use(userRouter)
 
 app.listen(port, () => console.log(`listening at port ${port}`))
