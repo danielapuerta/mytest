54405cbcc8cb57440df4b546ab0f5bae472e10d3
danielapuerta <danielapuerta151@gmail.com>
2023-07-05 14:45:58 +0100

    create post request for username and hashing password

--- routes/user.js
+++ routes/user.js
@@ -4,4 +4,23 @@ const database = require('../db/db')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 
+router.post("/users", ( request, response ) => {
+    const { user } = request.body
+    bcrypt.hash(user.password, 12)
+        .then(hashed_password => {
+           return database("users")
+                .insert({
+                    username: user.username,
+                    password_hash: hashed_password
+                }) 
+                .returning("*")
+                .then(users => {
+                    const user = users[0]
+                    response.json({ user })
+                }).catch(error => {
+                    response.json({ error: error.message })
+                })
+        })
+})
+
 module.exports = router;
