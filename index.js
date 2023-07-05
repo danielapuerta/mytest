d0768b5a6c2347675c10134392296f59f56ce393
danielapuerta <danielapuerta151@gmail.com>
2023-07-05 19:33:14 +0100

    defining my user and index routes

--- index.js
+++ index.js
@@ -4,23 +4,26 @@ const cors = require('cors')
 var path = require('path');
 
 const userRouter = require('./routes/user')
+const indexRouter = require('./routes/index')
 
+//define my server as 'app'
 const app = express();
-
-app.use(bodyParser.json())
-app.use(cors())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-/* GET home page. */
-app.get('/', function(req, res, next) {
-    res.render('index', { title: 'SeniorCare' });
-  });
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+
 
 const port = 5000
 
 app.listen(port, () => console.log(`listening at port ${port}`))
 
 app.use(userRouter)
+app.use(indexRouter)
