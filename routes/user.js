47d4119f2f6c39054fce5dd58ab4b663e53d01df
danielapuerta <danielapuerta151@gmail.com>
2023-08-18 12:06:21 +0100

    commenting for authentication in Login (using JWT)

--- routes/user.js
+++ routes/user.js
@@ -38,25 +38,31 @@ router.post("/api/register", (request, response, next) => {
     })
  })
 
- 
+
  router.post("/api/authen/login", (request, response, next) => {
+   //user logs in and first check is
    database("users")
+   //select from the users table where username in db == to username from the request body 
    .where({username: request.body.username})
    .first()
    .then(user => {
+      //if user is not found, send an error response
       if(!user){
          response.status(401).json({
             error: "No user by that name"
          })
       }else{
+         //if it's found, compare the password from the req body with the hashed pass store in the db
          return bcrypt
          .compare(request.body.password, user.password_hash)
+         //create a Promise where if is not Authenticated send an error response
          .then(isAuthenticated => {
             if(!isAuthenticated){
                response.status(401).json({
                   error: "Unauthorized Access!"
                })
             }else{
+               //if it's authenticated, create and return a token with the user found
                return jwt.sign(user, SECRET, (error, token) => {
                   response.status(200).json({token})
                })
