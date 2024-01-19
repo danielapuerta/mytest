55ca8d728b57daa375151534f7e950d359c4f024
danielapuerta <danielapuerta151@gmail.com>
2024-01-19 18:39:35 +0000

    verify token in the residentList page, use next() to continue with next middleware

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const jwt = require('jsonwebtoken')
+const secret =  "SECRET"
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
   res.render('index', { title: 'SeniorCare' });
 });
 
@@ -14,8 +17,26 @@ router.get('/register', function(req, res, next) {
   res.render('register', { title: 'SeniorCare' });
 });
 
-router.get('/residentsList', function(req, res, next) {
+router.get('/residentsList', authenticateUser, function(req, res, next) {
+  
   res.render('residentsList', { title: 'SeniorCare' });
 });
+
+
+
+function authenticateUser(request, response, next) {
+  let token = request.cookies["x-access-token"];
+  console.log(token)
+  jwt.verify(token, secret, (error, decodedToken) => {
+     if(error){
+        response.status(401).json({
+           message: "Unauthorized Access!"
+        })
+     }else{
+      //executes the next middleware 
+      next()
+     }
+  })
+}
 
 module.exports = router;
